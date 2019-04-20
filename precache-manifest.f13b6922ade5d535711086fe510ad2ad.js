cc6dc71f079bac96902980b336fe658572793613
Nguyen Phat <phatnt.uit@gmail.com>
2019-04-20 19:18:40 +0700

    Updates

--- precache-manifest.f13b6922ade5d535711086fe510ad2ad.js
+++ precache-manifest.f13b6922ade5d535711086fe510ad2ad.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/use-react-hooks/static/js/runtime~main.cf9da9da.js"
   },
   {
-    "revision": "3c456601ee1c75736730",
-    "url": "/use-react-hooks/static/js/main.9771cd66.chunk.js"
+    "revision": "368472f196ca57a1be04",
+    "url": "/use-react-hooks/static/js/main.3a2b77b7.chunk.js"
   },
   {
     "revision": "0ccf59f247c71239974d",
     "url": "/use-react-hooks/static/js/2.2db90dec.chunk.js"
   },
   {
-    "revision": "3c456601ee1c75736730",
-    "url": "/use-react-hooks/static/css/main.3851db97.chunk.css"
+    "revision": "368472f196ca57a1be04",
+    "url": "/use-react-hooks/static/css/main.6f877904.chunk.css"
   },
   {
-    "revision": "3fef069bff80520b13eb14d3834ba95e",
+    "revision": "a4ee13ab57acbfe6cefe0956ab46a447",
     "url": "/use-react-hooks/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/use-react-hooks/precache-manifest.91e32c1ad67b5d9bc52837efdac8db36.js"
+  "/use-react-hooks/precache-manifest.f13b6922ade5d535711086fe510ad2ad.js"
 );
 
 workbox.clientsClaim();
